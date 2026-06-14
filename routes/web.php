e23d82c5cbd9251ed6557e6f37b95050f9568b77
Mohamad <mohamadamin_dastmozdraki@outlook.com>
2026-06-14 12:37:26 +0330

    Add playground route with validation for dynamic views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/playground/{s}', function (string $s) {
+    abort_unless(preg_match('/^[a-z0-9-]+$/', $s), 404);
+
+    return view()->exists("playground.{$s}")
+        ? view("playground.{$s}")
+        : abort(404);
+});
